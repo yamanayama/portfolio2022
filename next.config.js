b2205d6aa8386e20bad19950e1b358f3489f34c4
yamanayama <ymmtnomail@gmail.com>
2023-03-09 13:08:18 +0900

    linariaとBableの実装

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withLinaria = require('next-linaria')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["images.microcms-assets.io"],
   },
-};
+}
 
-module.exports = nextConfig;
+const linariaOption = {}
+
+module.exports = withLinaria({
+  ...nextConfig,
+  linaria: linariaOption,
+})
