141fa3b4ec479c82d297d809613507f0f9ad4080
yamanayama <ymmtnomail@gmail.com>
2023-03-19 16:38:53 +0900

    common系のスタイルの追加・整理

---
+++ styles/constans.js
@@ -0,0 +1,53 @@
+export const breakPoint = "751px";
+export const contentWidth = "1160px";
+
+export const color = {
+  black: "#000",
+  white: "#fff",
+  error: "#B00020",
+  background: "#FFF2EC",
+  surface: "#FFF2EC",
+
+  text: {
+    high: "rgba(0,0,0,0.87)",
+    medium: "rgba(0,0,0,0.6)",
+    disabled: "rgba(0,0,0,0.38)"
+  },
+  blue: {
+    30: "#E3EEFF",
+    50: "#0062FF",
+    60: "#0062FF",
+    80: "#005DF2"
+  },
+  pink: {
+    30: "#FFEEEF",
+    50: "#FF8D90",
+    60: "#FF7C80",
+    80: "#FF5A5F",
+  },
+  gray: {
+    10: "#FAFAFA",
+    20: "#FEFEFE",
+    30: "#F1F3F4",
+    40: "#E3E5E5",
+    50: "#C6CACC",
+    60: "#919699",
+    70: "#5E6366",
+    80: "#3E4345"
+  },
+};
+
+export const typography = {
+  text: {
+    headline1: "96",
+    headline2: "60",
+    headline3: "48",
+    headline4: "34",
+    headline5: "24",
+    headline6: "20",
+    body1: "16",
+    body2: "14",
+    overline: "10",
+    caption: "12",
+  }
+} 
