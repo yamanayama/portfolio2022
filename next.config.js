8fa5e9c9f59276606e2b690a2fd874464abdf1c1
yamanayama <ymmtnomail@gmail.com>
2023-04-20 00:37:03 +0900

    svgを使えるようにする

--- next.config.js
+++ next.config.js
@@ -10,7 +10,18 @@ const nextConfig = {
   },
   compiler: {
     emotion: true
-  }
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+        },
+      ],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
