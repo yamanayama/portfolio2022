c9c9efc6ed95bdf5be1f9cabcbdc8307c17676b4
yamanayama <ymmtnomail@gmail.com>
2023-05-07 00:11:08 +0900

    ogpの設定

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja" prefix="og: http://ogp.me/ns#">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600;700;800&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
